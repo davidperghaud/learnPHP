bf5819623e0a1a767e16176f482d5a71f7c47556
davidperghaud <dmboyid@gmail.com>
2019-03-12 17:50:53 +0100

    ajout des fichiers info.php et test.php

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Page de Test</h1>
+
+	<p>
+		Cette page contient du code HTML<br/>
+		<?php echo "Bonjour!" ?>
+		Voici quelques petits tests
+	</p>
+
+	<ul>
+		<li style="color: blue">Texte en bleu</li>
+		<li style="color: red">Texte en rouge</li>
+		<li style="color: green">Texte en vert</li>
+	</ul>
+
+	<?php  ?>
+
+</body>
+</html>
