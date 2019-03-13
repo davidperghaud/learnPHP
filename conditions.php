fa9a12269e09fa95990e45e1b44c3a7dcde1300c
davidperghaud <dmboyid@gmail.com>
2019-03-13 11:49:34 +0100

    cours sur les conditions termine

---
+++ conditions.php
@@ -0,0 +1,23 @@
+<?php
+
+$age = 18;
+
+switch ($age) {
+    case 4:
+        echo "Tu as 4 ans";
+        break;
+
+    case 16:
+        echo "Tu es un peu plus âgé, tu as 16 ans";
+        break;
+
+    case 18:
+        echo "Tu es majeur !";
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+?>
