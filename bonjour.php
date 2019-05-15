f7e995dca87d8eaf4e8fa6eab66530eb6f9c9791
davidperghaud <dmboyid@gmail.com>
2019-05-15 12:05:38 +0100

    transmission par url

--- bonjour.php
+++ bonjour.php
@@ -1,5 +1,6 @@
 <!DOCTYPE <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,11 +9,35 @@
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
     <script src="main.js"></script> -->
 </head>
+
 <body>
-    
-<p>
+
+    <!-- <p>
     Bonjour <?php echo $_GET['nom']. " " .$_GET['prenom'] ?>
-</p>
+</p> -->
+
+    <?php
+
+if(isset($_GET['prenom']) AND isset($_GET['nom']) AND isset($_GET['repeter'])){
+    // on force la conversion en nombre entier
+    $_GET['repeter'] = (int)$_GET['repeter'];
+
+    //le nomrbre doit etre compris entre 0 et 100
+    if ($_GET['repeter'] >= 1 AND $_GET['repeter'] <= 100) {
+       for ($i=0; $i < $_GET['repeter']; $i++) { 
+        echo 'Bonjour '. $_GET['prenom'] . ' '. $_GET['nom']. ' !<br />';
+        } 
+    }
+
+    
+}
+else
+{
+    echo "Il faut renseigner un prénom et un nom et un nombre de repetitions !";
+}
+
+?>
 
 </body>
+
 </html>
